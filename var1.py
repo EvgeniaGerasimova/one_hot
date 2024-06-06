b7af4740a566d65b6830d45c54b8cdf99556ffa9
Evgenia Gerasimova <semchenko.jenia@yandex.ru>
2024-06-06 16:55:00 +0300

    перевод в one hot вид

---
+++ var1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10 + ['human'] * 10
+random.shuffle(lst)
+
+data = pd.DataFrame({'whoAmI': lst})
+
+for mean in data['whoAmI'].unique():
+    data[mean] = data['whoAmI'].apply(lambda x: 1 if x == mean else 0)
+
+data = data.drop('whoAmI', axis=1)
+
+data.head()
---
+++ var2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10 + ['human'] * 10
+random.shuffle(lst)
+
+data = pd.DataFrame({'whoAmI': lst})
+
+new_data = pd.DataFrame()
+
+values = data['whoAmI'].unique()
+
+for mean in values:
+    new_data[mean] = (data['whoAmI'] == mean).astype(int)
+
+new_data.head()
